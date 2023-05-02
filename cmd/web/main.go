ff66cb0eb50bde356a4a4de415de38053d04490b
lipandr <andrey.lipayev@gmail.com>
2023-05-02 16:10:38 +0300

    Managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Directory containing static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
@@ -15,6 +27,6 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on port 4000")
-	log.Fatal(http.ListenAndServe(":4000", mux))
+	log.Printf("Starting server on port %s\n", cfg.addr)
+	log.Fatal(http.ListenAndServe(cfg.addr, mux))
 }
