17cd7d4387a85c6d60351ffeb2ad633c6742f8a0
lipandr <andrey.lipayev@gmail.com>
2023-05-06 15:47:08 +0300

    Isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,21 +7,15 @@ import (
 	"os"
 )
 
-type config struct {
-	addr      string
-	staticDir string
-}
-
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
 }
 
 func main() {
-	var cfg config
 
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Directory containing static assets")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -33,7 +27,7 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
@@ -42,11 +36,11 @@ func main() {
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	srv := &http.Server{
-		Addr:     cfg.addr,
-		Handler:  mux,
+		Addr:     *addr,
+		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
 
-	infoLog.Printf("Starting server on port %s\n", cfg.addr)
+	infoLog.Printf("Starting server on port %s\n", *addr)
 	errorLog.Fatal(srv.ListenAndServe())
 }
