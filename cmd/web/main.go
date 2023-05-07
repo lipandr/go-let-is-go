8e999fef0467ac7b10d23fcfab1c53823dbb772a
lipandr <andrey.lipayev@gmail.com>
2023-05-07 22:31:38 +0300

    Creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var dbConnCounts int
 
 type application struct {
 	infoLog  *log.Logger
@@ -20,6 +26,15 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db := connectDB()
+	if db == nil {
+		errorLog.Fatal("Failed to connect to MySQL database")
+	}
+
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
 
 	app := &application{
 		infoLog:  infoLog,
@@ -43,4 +58,35 @@ func main() {
 
 	infoLog.Printf("Starting server on port %s\n", *addr)
 	errorLog.Fatal(srv.ListenAndServe())
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func connectDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for {
+		conn, err := openDB(dsn)
+		if err != nil {
+			log.Println("MySQL is not yet ready...")
+			dbConnCounts++
+		} else {
+			log.Println("MySQL is ready and connected!")
+			return conn
+		}
+		if dbConnCounts > 10 {
+			log.Println(err)
+			return nil
+		}
+		log.Println("Retrying in 2 seconds...")
+		time.Sleep(2 * time.Second)
+	}
 }
